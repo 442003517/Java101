67507d7e1cd1416254e27870d1a669829307b029
442003517 <91524985+442003517@users.noreply.github.com>
2021-12-21 23:10:27 +0300

    Create Stars.java

---
+++ Stars.java
@@ -0,0 +1,13 @@
+public class main
+{
+  public static void main(String[] args) {
+    for (int i=0;i <= 4 ; i++)
+    {
+      for (int j=0;j<=i ; j++)
+      {
+        System.out.println("a");
+      }
+      System.out.println();
+    }
+  }
+}
